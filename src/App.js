5f6eadbfcbb82b5f4eae839b05d248e4ce812c51
Darrin Lowery <dlowery@monsto.com>
2020-04-01 15:55:31 -0500

    demo cleanup

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import 'semantic-ui-css/semantic.min.css'
 import './themes/original/style.scss';
 
 // Pages
-import Demo from "./pages/Demo"
 import Frame from "./pages/Frame"
-import Page from "./pages/Page"
+// import Demo from "./pages/Demo"
+// import Page from "./pages/Page"
 
 function App() {
 	return (
@@ -15,8 +15,8 @@ function App() {
 			<div className="App">
 				<Switch>
 					<Route exact path="/" component={Frame} />
-					<Route exact path="/demo" component={Demo} />
-					<Route exact path="/page" component={Page} />
+					{/* <Route exact path="/demo" component={Demo} />
+					<Route exact path="/page" component={Page} /> */}
 					<Redirect from="*" to="/" component={Frame} />
 				</Switch>
 			</div>
